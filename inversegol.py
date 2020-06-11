6e5f05a1a391fcafe88b2c778661c37c3cfce408
ngetachew <natugetachew@gmail.com>
2020-06-11 13:05:27 -0700

    Added inversegol which just reverses some rules and cleaned up the files a little bit. Also, now the starting points are read from a text file

--- inversegol.py
+++ inversegol.py
@@ -4,15 +4,15 @@ import random
 
 CELL_COLOR = (132, 177, 219)
 BLACK = (0,0,0)
-size = (900, 900)
+size = (700, 700)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Conways Game of Life')
 screen.fill(BLACK)
 GENERATION = 0
 
-num = size[0]//25 - 1 #square number of cells, using cells with pixel size 25
+num = size[0]//25 #for 28x28 pixel cells
 #Rectangle Objects
-rects = [list([pygame.Rect(x,y,25,25) for y in range(0,size[0],25)]) for x in range(0,size[0],25)]
+rects = [list([pygame.Rect(x,y,25,25) for y in range(0,700,25)]) for x in range(0,700,25)]
 #Map of which cells are alive and which are dead
 state_map = [list([0 for r in range(len(rects))]) for c in range(len(rects[0]))] 
 
@@ -26,13 +26,12 @@ with open('startingpoints.txt','r',encoding = 'utf-8') as f:
 
 for (r,c) in start_pos:
     state_map[int(r)][int(c)] = 1
-    
 #Main loop
 done = False
 while not done:
     pygame.time.wait(1000)
     screen.fill(BLACK)
-    pygame.display.set_caption('Conways Game of Life. Generation: ' + str(GENERATION))
+    print(GENERATION)
     GENERATION += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,16 +50,16 @@ while not done:
             num_neighbors = 0
             r_shift = [-1,1,0]
             c_shift = [-1,1,0]
-            if r%num==0:
-                if r/num == 0:
-                    r_shift[0] = num
-                elif r/num == 1:
-                    r_shift[1] = -num
-            if c%num==0:
-                if c/num==0:
-                    c_shift[0] = num
-                elif c/num == 1:
-                    c_shift[1] = -num
+            if r%27==0:
+                if r/27 == 0:
+                    r_shift[0] = 27
+                elif r/27 == 1:
+                    r_shift[1] = -27
+            if c%27==0:
+                if c/27==0:
+                    c_shift[0] = 27
+                elif c/27 == 1:
+                    c_shift[1] = -27
 
             for i in r_shift:
                 for j in c_shift:
@@ -69,12 +68,12 @@ while not done:
                     else:
                         num_neighbors += state_map[r+i][c+j]
         #Check rules    
-            if state_map[r][c] == 1:
+            if state_map[r][c] == 0:
                 if num_neighbors < 2 or  num_neighbors > 3: 
-                    changes[(r,c)]= 0
-            elif state_map[r][c] == 0:
-                if num_neighbors == 3:
                     changes[(r,c)]= 1
+            elif state_map[r][c] == 1:
+                if num_neighbors == 3:
+                    changes[(r,c)]= 0
     #Swap in new states
     for c in changes:
         r,c = c[0],c[1]
